cca44093892a8f4b0fb7680870a6aab82f76282c
kunal <109961679+Kuncode2@users.noreply.github.com>
2023-12-27 12:40:10 +0530

    Add files via upload

---
+++ javascript-practice/map_filter_reduce.js
@@ -0,0 +1,84 @@
+// map, filter ,reduce are the array methods 
+
+// map() = creating a new array by exiting one
+
+// const num = [1,2,3,4]
+
+// const fivetime = num.map((a,i,num) => {
+//     return a*5 + i
+
+// })
+// console.log(fivetime);
+
+// //  ===========================
+
+// filter() - It takes each element  in an array and apply an condition on it if the condtion is true then it will give true or false
+// in short it fullfill the criteria
+
+// const a = [1,2,3,4]
+// const less2 = a.filter((z)=>{
+//     return z > 2;
+// })
+// console.log(less2);
+
+// // ============================
+
+//reduce() ->  it is use to reduce the array and give the singe output of an array 
+
+// const a = [1,2,3,4]
+// const b = a.reduce((acc,curr,i,arr)=>{      in this code the acc is the initial value
+//     return acc+curr
+// },0)
+// console.log(b);
+
+//======= QUESTIONS ===========
+// Q-1 => return only name of student in captical
+
+// let student = [
+//     {name:"kunal",rollNumber:24,marks:60},
+//     {name:"kuna",rollNumber:2,marks:6},
+//     {name:"kun",rollNumber:4,marks:0},
+//     {name:"kun",rollNumber:26,marks:61},
+// ];
+
+// let list2 = student.map((names)=> names.name.toUpperCase())
+// console.log(list2);
+
+//----------------------------------------------------------------
+
+// Q-2 => Return only details of those who scored more than 60 marks
+
+// let scor = student.filter((scored) => scored.marks > 60)
+// console.log(scor);
+//------------------------------------------------------------------
+
+//Q-3 => More than 60 marks and rollNumber greater than 15
+
+// let details = student.filter((det)=>det.marks > 60 && det.rollNumber > 15)
+// console.log(details)
+//------------------------------------------------------------------
+
+//Q-4 => Sum of makrs of all student
+
+// let a = student.reduce((ini,curr)=> ini+curr.marks,0)
+// console.log(a);
+
+//Q-5 => Return only names of student who scored more than 60 
+
+// const a = student.filter((store) => store.marks > 10).map((store) => store.name);
+// console.log(a);
+
+//Q-6 => return total marks for students with mars grater than 60 after 20 marks have been added to those scored less than 60
+
+// const a = student
+// .map((mar)=>{
+//     if(mar.marks<60){
+//         mar.marks += 20
+//     }
+//     return mar
+// })
+
+// .filter((mar) => mar.marks > 60)
+// .reduce((ini,curr) => ini+curr.marks,0)
+
+// console.log(a);
---
+++ javascript-practice/var_let_const.js
@@ -0,0 +1,63 @@
+// //scope
+//  var a= 2  // This is the global scope 
+//  function a(){
+    // This is the functional scope
+//  } 
+//  {
+//     //This is the blog scope
+//  }
+
+ // var is functional scope but the const and the let is block scope 
+
+//  var v = 2
+//  {
+//  console.log(v) // the out but is the v // now thw variable is in block scope so the answer is same
+//  }
+//  //condition
+//  {let q = 3 }
+//  console.log(q) // it will give error and this same for the const
+//==============================================================
+ 
+//shadowing and illegical shadowing
+
+// function a(){
+//     var a = "b"
+//     let b = "a"
+//     if(true){
+//         let a = "a"
+//         var b = "z"
+//         console.log(a)
+//     }
+//     console.log(a)
+// }
+// a()
+
+// ===================================================================================
+// declartion 
+// // we can declare a variable multipe time  with using var but  not in let and const
+// let a =55
+// {
+//     let a = 2
+// }
+// console.log(a)
+
+// Decelartion without initinislation
+//  var kunal // valid
+//  let kunal // vaild
+// const kunal // const decleration must be intialized  // assigment to a constant variabl
+//...............................................................................
+
+
+//        =======           HOSTING     ============= 
+
+// moving the function and decelartion to the top of the code is called  hosting
+
+// var a    // this what the javascript do 
+// console.log(a)   // this will give undefined
+// a = 2
+
+// for lat and const  -- this will give error that we canot give value before we inilitize it  
+//  they are hosted in temporary dead zone -(variables declared with let and const exist but cannot be accessed or assigned any valu)
+ 
+
+
